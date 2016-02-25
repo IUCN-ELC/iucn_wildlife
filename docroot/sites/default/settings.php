5ca691346bf312b385edc0137ab78590c642ede9
Octavian Belu <belu.octavian@yahoo.com>
2016-02-25 17:08:48 +0200

    Configured sync_directory in settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -244,7 +244,9 @@ $databases = array();
  *   );
  * @endcode
  */
-$config_directories = array();
+$config_directories = array(
+  'CONFIG_SYNC_DIRECTORY' => 'sites/default/config',
+);
 
 /**
  * Settings:

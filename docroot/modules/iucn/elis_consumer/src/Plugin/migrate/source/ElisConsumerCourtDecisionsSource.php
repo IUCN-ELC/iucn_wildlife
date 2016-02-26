20b387c7f1aa432736c5e8908f8bb331b1bdb816
Octavian Belu <belu.octavian@yahoo.com>
2016-02-26 12:36:05 +0200

    Configured court decisions migration

---
+++ docroot/modules/iucn/elis_consumer/src/Plugin/migrate/source/ElisConsumerCourtDecisionsSource.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\elis_consumer\Plugin\migrate\source\ElisConsumerCourtDecisionsSource.
+ */
+
+namespace Drupal\elis_consumer\Plugin\migrate\source;
+
+use Drupal\migrate_source_json\Plugin\migrate\source\JSONSource;
+
+/**
+ * Migrate court decision from ELIS database.
+ *
+ * @MigrateSource(
+ *   id = "elis_consumer_court_decisions"
+ * )
+ */
+class ElisConsumerCourtDecisionsSource extends JSONSource {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, \Drupal\migrate\Entity\MigrationInterface $migration, array $namespaces = array()) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $namespaces);
+  }
+}

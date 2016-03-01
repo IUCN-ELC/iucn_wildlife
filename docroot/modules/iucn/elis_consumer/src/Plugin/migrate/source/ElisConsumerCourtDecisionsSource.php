1c81d8a43d0f9dd675b9b945d0fe7cff1f1e41b8
Octavian Belu <belu.octavian@yahoo.com>
2016-03-01 17:15:59 +0200

    Created a request for each month if year has more than 700 results

--- docroot/modules/iucn/elis_consumer/src/Plugin/migrate/source/ElisConsumerCourtDecisionsSource.php
+++ docroot/modules/iucn/elis_consumer/src/Plugin/migrate/source/ElisConsumerCourtDecisionsSource.php
@@ -63,7 +63,7 @@ class ElisConsumerCourtDecisionsSource extends SourcePluginBase {
     $this->identifier = 'id';
     
     $this->client = \Drupal::httpClient();
-    $this->date_period = $this->get_date_period();
+    $this->date_period = $this->get_date_period('Y*', '1 year');
   }
 
   public function hexadecimally_encode_string($str) {
@@ -71,10 +71,10 @@ class ElisConsumerCourtDecisionsSource extends SourcePluginBase {
     return reset($unpack);
   }
 
-  public function get_date_period($format = 'Y', $interval = '1 year') {
+  public function get_date_period($format, $interval, $start_date = NULL, $end_date = NULL) {
     $return = array();
-    $begin = new \DateTime($this->start_date);
-    $end = new \DateTime();
+    $begin = $start_date == NULL ? new \DateTime($this->start_date) : new \DateTime($start_date);
+    $end = $end_date == NULL ? new \DateTime() : new \DateTime($end_date);
 
     $interval = \DateInterval::createFromDateString($interval);
     $period = new \DatePeriod($begin, $interval, $end);
@@ -199,22 +199,21 @@ class ElisConsumerCourtDecisionsSource extends SourcePluginBase {
       array($spage_query,$spage_first),
       $url
     );
+    print_r($url . PHP_EOL);
     $response = $this->getResponse($url);
     $data = trim(utf8_encode($response->getBody()));
     return simplexml_load_string($data);
   }
 
-  public function getSourceData($url) {
-    $query = $this->spage_query_default_string . ' AND DM:' . current($this->date_period) . '*';
-    $spage_query = $this->hexadecimally_encode_string($query);
-    $spage_first = 0;
-    try {
+  public function getDocumentsByMonth($year) {
+    $date_period = $this->get_date_period('Ym', '1 month', $year . '-01', ($year + 1) . '-01');
+    $docs = [];
+    while (key($date_period) !== NULL) {
+      $query = $this->spage_query_default_string . ' AND DM:' . current($date_period);
+      $spage_query = $this->hexadecimally_encode_string($query);
+      $spage_first = 0;
       $xml = $this->getElisXml($url, $spage_query, $spage_first);
       $numberResultsFound = (int)$xml->attributes()->numberResultsFound;
-      $docs = [];
-      $json = json_encode($xml->attributes());
-      // The TRUE setting means decode the response into an associative array.
-      $array = json_decode($json, TRUE);
       while (TRUE) {
         foreach ($xml->document as $doc) {
           $docs[(string) $doc->{$this->identifier}] = (array)$this->getItem($doc);
@@ -224,6 +223,38 @@ class ElisConsumerCourtDecisionsSource extends SourcePluginBase {
           break;
         }
         $xml = $this->getElisXml($url, $spage_query, $spage_first);
+      }
+      next($date_period);
+    }
+    return $docs;
+  }
+
+  public function getSourceData($url) {
+    $query = $this->spage_query_default_string . ' AND DM:' . current($this->date_period);
+    $spage_query = $this->hexadecimally_encode_string($query);
+    $spage_first = 0;
+    try {
+      $xml = $this->getElisXml($url, $spage_query, $spage_first);
+      $numberResultsFound = (int)$xml->attributes()->numberResultsFound;
+      $numberResultsPresented = (int)$xml->attributes()->numberResultsPresented;
+      $docs = [];
+      $json = json_encode($xml->attributes());
+      // The TRUE setting means decode the response into an associative array.
+      $array = json_decode($json, TRUE);
+      if ($numberResultsFound > $numberResultsPresented) {
+        $docs = $this->getDocumentsByMonth(substr(current($this->date_period), 0, 4));
+      }
+      else {
+        while (TRUE) {
+          foreach ($xml->document as $doc) {
+            $docs[(string) $doc->{$this->identifier}] = (array)$this->getItem($doc);
+          }
+          $spage_first += 20;
+          if ($spage_first >= $numberResultsFound) {
+            break;
+          }
+          $xml = $this->getElisXml($url, $spage_query, $spage_first);
+        }
       }
       $array['documents'] = $docs;
       return $array;

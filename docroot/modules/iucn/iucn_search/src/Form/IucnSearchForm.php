a4679c702fd478691fc00979084c0536fd52ceae
beluoctavian <beluoctavian@gmail.com>
2016-03-08 16:49:03 +0200

    Implemented getSearchResults method

--- docroot/modules/iucn/iucn_search/src/Form/IucnSearchForm.php
+++ docroot/modules/iucn/iucn_search/src/Form/IucnSearchForm.php
@@ -9,6 +9,7 @@ namespace Drupal\iucn_search\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\search_api\Entity\Index;
 
 class IucnSearchForm extends FormBase {
 
@@ -25,15 +26,21 @@ class IucnSearchForm extends FormBase {
    * {@inheritdoc}.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $text = !empty($_GET[$this->search_url_param]) ? $_GET[$this->search_url_param] : '';
     $form['text'] = [
       '#type' => 'textfield',
       '#title' => 'Search text',
-      '#default_value' => !empty($_GET[$this->search_url_param]) ? $_GET[$this->search_url_param] : '',
+      '#default_value' => $text,
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => 'Search',
     ];
+    if (!empty($text)) {
+      $form['results'] = [
+        '#markup' => $this->getSeachResults($text),
+      ];
+    }
     return $form;
   }
 
@@ -49,6 +56,24 @@ class IucnSearchForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $search_text = $form_state->getValue('text');
     $form_state->setRedirect('iucn.search', [], ['query' => [$this->search_url_param => $search_text]]);
+  }
+
+  private function getSeachResults($search_text) {
+    $index = Index::load('default_node_index');
+    $query = $index->query();
+    $query->keys($search_text);
+    $resultSet = $index->getServerInstance()->search($query);
+
+    $results = [];
+
+    foreach ($resultSet->getResultItems() as $item) {
+      $item_nid = $item->getField('nid')->getValues()[0];
+      $results[$item_nid] = [
+        'nid' => $item_nid,
+      ];
+      break;
+    }
+    return $results;
   }
 
 }

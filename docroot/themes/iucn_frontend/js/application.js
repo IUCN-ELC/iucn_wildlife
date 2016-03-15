94446641c1683215d4110a10559f0e33c405fba5
beluoctavian <beluoctavian@gmail.com>
2016-03-15 17:07:10 +0200

    Submit the form when changing 'operator' checkbox

--- docroot/themes/iucn_frontend/js/application.js
+++ docroot/themes/iucn_frontend/js/application.js
@@ -1,5 +1,8 @@
 jQuery('select').select2();
 
-jQuery('select', '#iucn-search-form').change(function() {
+jQuery('select', '#iucn-search-form').change(submitSearchForm);
+jQuery('input[type="checkbox"]', '#iucn-search-form').change(submitSearchForm);
+
+function submitSearchForm() {
     jQuery('#iucn-search-form').submit();
-});
+}

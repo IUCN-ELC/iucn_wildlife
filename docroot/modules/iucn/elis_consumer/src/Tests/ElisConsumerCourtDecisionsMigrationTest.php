bb4ec5334e1ab0da7addfd7dd0d6958e99ed20ae
beluoctavian <beluoctavian@gmail.com>
2016-03-22 17:13:20 +0200

    Started implementation of migration tests

---
+++ docroot/modules/iucn/elis_consumer/src/Tests/ElisConsumerCourtDecisionsMigrationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\elis_consumer\Tests;
+
+use Drupal\simpletest\WebTestBase;
+use Drupal\migrate\Entity\Migration;
+use Drupal\migrate_tools\MigrateExecutable;
+use Drupal\migrate_tools\DrushLogMigrateMessage;
+
+/**
+ * Test migrations from ELIS.
+ *
+ * @group Elis Consumer
+ */
+class ElisConsumerMigrationTest extends WebTestBase {
+
+  public static $modules = ['iucn_search'];
+  protected $profile = 'standard';
+
+  /**
+   * @var \Drupal\migrate\Entity\Migration
+   */
+  private $couMigration;
+
+  private $migrateExecutable;
+
+  public function setUp() {
+    parent::setUp();
+    $this->couMigration = Migration::load('elis_consumer_court_decisions');
+    $options = [
+      'limit' => 10,
+    ];
+    $log = new DrushLogMigrateMessage();
+    $sourcePlugin = $couMigration->getSourcePlugin();
+    $path = drupal_get_path('module', 'elis_consumer') . '/src/Tests/data/CourtDecisions.xml';
+    $sourcePlugin->setPath($path);
+    $this->migrateExecutable = new MigrateExecutable($couMigration, $log, $options);
+  }
+
+  public function testCouMigration1() {
+  }
+
+}

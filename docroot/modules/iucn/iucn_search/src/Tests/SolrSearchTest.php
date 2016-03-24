ed1b680a980bfe55c7aa1720f9b81afdc31d0d66
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-03-24 21:28:21 +0200

    Testing facets combinations and basic search word

--- docroot/modules/iucn/iucn_search/src/Tests/SolrSearchTest.php
+++ docroot/modules/iucn/iucn_search/src/Tests/SolrSearchTest.php
@@ -34,7 +34,7 @@ class SolrSearchTest extends WebTestBase {
     $default_index->set('server', 'iucn_search_test');
     $default_index->save();
     // TODO - this cleanup index not working - deleting nodes in tear down.
-    $default_index->getServerInstance()->removeIndex($default_index);
+//    $default_index->getServerInstance()->removeIndex($default_index);
     // Create test content.
     $this->createTestContent();
     // Give SOLR some time to commit.
@@ -46,6 +46,7 @@ class SolrSearchTest extends WebTestBase {
     foreach ($this->nodes as $node) {
       $node->delete();
     }
+    sleep(5);
   }
 
   private function createTestContent() {
@@ -56,6 +57,15 @@ class SolrSearchTest extends WebTestBase {
       $term->save();
       if (!empty($term->id())) {
         $this->terms[] = $term;
+      }
+    }
+
+    $ref_nodes = Yaml::decode(file_get_contents($module_path . '/data/ReferencedNodes.yml'));
+    foreach ($ref_nodes as $edit_node) {
+      $node = Node::create($edit_node);
+      $node->save();
+      if (!empty($node->id())) {
+        $this->nodes[$node->id()] = $node;
       }
     }
 
@@ -76,7 +86,7 @@ class SolrSearchTest extends WebTestBase {
   public function testSearch() {
     // Test total nodes created.
     $nodes = \Drupal\node\Entity\Node::loadMultiple(array_keys($this->nodes));
-    $this->assertEqual(3, count($nodes), 'Nodes Created');
+    $this->assertEqual(13, count($nodes), 'Nodes Created');
 
     // Get search server.
     $search_server = new SolrSearchServer('default_node_index');
@@ -84,8 +94,7 @@ class SolrSearchTest extends WebTestBase {
     // Test empty search - all results.
     $search = new SolrSearch(array(), $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(3, $result->getCountTotal(), 'Nodes indexed');
-
+    $this->assertEqual(8, $result->getCountTotal(), 'Nodes indexed');
 
 
     // Test ecolex subject.
@@ -95,40 +104,314 @@ class SolrSearchTest extends WebTestBase {
     );
     $search = new SolrSearch($params, $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(2, $result->getCountTotal(), 'Filter by ecolex subject 1 OR');
-
-    // Test ecolex subject facet - Multiple values OR.
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' OR');
     $params = array(
-      'field_ecolex_subjects' => '1,2,3',
+      'field_ecolex_subjects' => '5',
     );
     $search = new SolrSearch($params, $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(3, $result->getCountTotal(), 'Filter by ecolex subject 1,2,3 OR');
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' OR');
+
+    // Test ecolex subject facet - Multiple values OR.
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(4, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' OR');
 
     // Test ecolex subject facet - Single values AND.
     $params = array(
       'field_ecolex_subjects' => '3',
+      'field_ecolex_subjects_operator' => 'AND',
     );
     $search = new SolrSearch($params, $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(2, $result->getCountTotal(), 'Filter by ecolex subject 3 AND');
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' AND');
 
     // Test ecolex subject facet - Multiple values AND.
     $params = array(
-      'field_ecolex_subjects' => '1,3',
+      'field_ecolex_subjects' => '1,2',
       'field_ecolex_subjects_operator' => 'AND',
     );
     $search = new SolrSearch($params, $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(1, $result->getCountTotal(), 'Filter by ecolex subject 1,2 AND');
+    $this->assertEqual(2, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' AND');
 
     $params = array(
-      'field_ecolex_subjects' => '1,2,3',
+      'field_ecolex_subjects' => '1,2,4',
       'field_ecolex_subjects_operator' => 'AND',
     );
     $search = new SolrSearch($params, $search_server);
     $result = $search->search(0, 10);
-    $this->assertEqual(0, $result->getCountTotal(), 'Filter by ecolex subject 1,2,3 AND');
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by ecolex subject ' . $params['field_ecolex_subjects'] . ' AND');
+
+
+    // Test country.
+    // Test country facet - Single value OR.
+    $params = array(
+      'field_country' => '5',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by country ' . $params['field_country'] . ' OR');
+
+    // Test country facet - Multiple value OR.
+    $params = array(
+      'field_country' => '1,2',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(6, $result->getCountTotal(), 'Filter by country ' . $params['field_country'] . ' OR');
+
+    // Test country facet - Multiple value AND.
+    $params = array(
+      'field_country' => '1,2',
+      'field_country_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(2, $result->getCountTotal(), 'Filter by country ' . $params['field_country'] . ' AND');
+
+    $params = array(
+      'field_country' => '1,2,4',
+      'field_country_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), 'Filter by country ' . $params['field_country'] . ' AND');
+
+    $params = array(
+      'field_country' => '1,2,3,4,5',
+      'field_country_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by country ' . $params['field_country'] . ' AND');
+
+
+
+    // Test type of text.
+    // Test type of text facet - Single value OR.
+    $params = array(
+      'field_type_of_text' => '9',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by type of text ' . $params['field_type_of_text'] . ' OR');
+
+    // Test type of text facet - Multiple value OR.
+    $params = array(
+      'field_type_of_text' => '9,10',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(4, $result->getCountTotal(), 'Filter by type of text ' . $params['field_type_of_text'] . ' OR');
+
+    // Test type of text facet - Single value AND.
+    $params = array(
+      'field_type_of_text' => '9',
+      'field_type_of_text_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by type of text ' . $params['field_type_of_text'] . ' AND');
+
+    // Test country facet - Multiple value AND.
+    $params = array(
+      'field_type_of_text' => '9,10',
+      'field_type_of_text_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), 'Filter by type of text ' . $params['field_type_of_text'] . ' AND');
+
+    $params = array(
+      'field_type_of_text' => '8,9,10',
+      'field_type_of_text_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by type of text ' . $params['field_type_of_text'] . ' AND');
+
+
+
+    // Test subdivisions.
+    // Test subdivisions facet - Single value OR.
+    $params = array(
+      'field_territorial_subdivisions' => '11',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by subdivsions ' . $params['field_territorial_subdivisions'] . ' OR');
+
+    // Test subdivisions facet - Multiple value OR.
+    $params = array(
+      'field_territorial_subdivisions' => '11,12,13',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(4, $result->getCountTotal(), 'Filter by subdivsions ' . $params['field_territorial_subdivisions'] . ' OR');
+
+    // Test subdivisions facet - Single value AND.
+    $params = array(
+      'field_territorial_subdivisions' => '11',
+      'field_territorial_subdivisions_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), 'Filter by subdivsions ' . $params['field_territorial_subdivisions'] . ' AND');
+
+    // Test subdivisions facet - Multiple value AND.
+    $params = array(
+      'field_territorial_subdivisions' => '11,12',
+      'field_territorial_subdivisions_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), 'Filter by subdivsions ' . $params['field_territorial_subdivisions'] . ' AND');
+
+    $params = array(
+      'field_territorial_subdivisions' => '11,12,13',
+      'field_territorial_subdivisions_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), 'Filter by subdivsions ' . $params['field_territorial_subdivisions'] . ' AND');
+
+
+
+
+    // Test combined facets.
+    $test_case = 'Ecolex subject single value OR + Country single value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1',
+      'field_country' => '1',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(2, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value OR + Country single value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,3',
+      'field_country' => '2',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value OR + Country multiple value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,3',
+      'field_country' => '1,3',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(4, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country single value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,3',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '2',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country single value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '2',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country multiple value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '1,2,3',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(2, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Type of text multiple value OR.';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_type_of_text' => '6',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country multiple value AND.';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '1,2,3',
+      'field_country_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value OR + Country multiple value OR + Type of text multiple value OR';
+    $params = array(
+      'field_ecolex_subjects' => '1,3',
+      'field_country' => '1,3',
+      'field_type_of_text' => '6,9',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(3, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country single value OR + Type of text multiple value OR';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_type_of_text' => '6,10',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country single value OR + Type of text single value OR';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '1',
+      'field_type_of_text' => '6',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), $test_case);
+
+    $test_case = 'Ecolex subject multiple value AND + Country single value OR + Type of text multiple value AND';
+    $params = array(
+      'field_ecolex_subjects' => '1,2',
+      'field_ecolex_subjects_operator' => 'AND',
+      'field_country' => '1',
+      'field_type_of_text' => '6,10',
+      'field_type_of_text_operator' => 'AND',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(0, $result->getCountTotal(), $test_case);
+
+    // Test search words.
+    $test_case = 'Search unique word in title';
+    $params = array(
+      'q' => 'ABCD',
+    );
+    $search = new SolrSearch($params, $search_server);
+    $result = $search->search(0, 10);
+    $this->assertEqual(1, $result->getCountTotal(), $test_case);
 
 
   }

971cdaaee2d52b0e0df00e3e0759d11231f92cd1
Cristian Romanescu <cristian@romanescu.ro>
2016-04-11 18:24:39 +0300

    Changed test script to add mandatory parameters

--- test.sh
+++ test.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
 php=`which php`
-
-what=""
-
-if [ -z $@ ]; then
-  echo "Running all project-related tests ..."
-  what="iucn_search"
-else
-  if [ "$@" == *\\* ]; then
-    what="--class $@"
-  else
-    what="--class Drupal\\iucn_search\\Tests\\$@"
-  fi
-fi
+what="$@"
 
 $php docroot/core/scripts/run-tests.sh --non-html --color --verbose $what
-#    --sqlite /tmp/iucnwildlifed8.sqlite \
+if [ ! "$?" -eq 0 ]; then
+  echo "Usage: ./test.sh --url URL <group GROUP | --class CLASS >, example: ./test.sh --url http://iucnwildlifed8.local.ro elis_consumer"
+  echo
+  echo "Available GROUPs and CLASSes:"
+  php docroot/core/scripts/run-tests.sh --list | grep -e "elis" -e "iucn"
+  exit -1
+fi

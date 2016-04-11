44d455869a4b347d8b85eb4b3c6f793841df14fc
Cristian Romanescu <cristian@romanescu.ro>
2016-04-11 23:09:36 +0300

    more examples on test.sh usage

--- test.sh
+++ test.sh
@@ -5,7 +5,10 @@ what="$@"
 
 $php docroot/core/scripts/run-tests.sh --non-html --color --verbose $what
 if [ ! "$?" -eq 0 ]; then
-  echo "Usage: ./test.sh --url URL <group GROUP | --class CLASS >, example: ./test.sh --url http://iucnwildlifed8.local.ro elis_consumer"
+  echo "Usage: ./test.sh --url URL <group GROUP | --class CLASS>"
+  echo
+  echo "Example 1 (test group): ./test.sh --url http://iucnwildlifed8.local.ro elis_consumer"
+  echo "Example 2 (test class): ./test.sh --url http://iucnwildlifed8.local.ro --class Drupal\\iucn_search\\Tests\\SolrFacetTest"
   echo
   echo "Available GROUPs and CLASSes:"
   php docroot/core/scripts/run-tests.sh --list | grep -e "elis" -e "iucn"

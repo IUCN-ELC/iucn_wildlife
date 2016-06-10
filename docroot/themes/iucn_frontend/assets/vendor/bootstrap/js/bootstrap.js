efba8c6002e39dd485b42b39a98de3f29af23d9b
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-06-10 17:16:44 +0300

    Updated bootstrap build

--- docroot/themes/iucn_frontend/assets/vendor/bootstrap/js/bootstrap.js
+++ docroot/themes/iucn_frontend/assets/vendor/bootstrap/js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (<none>)
- * Config saved to config.json and <none>
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=23d4836115a7f1cea15090f28a1ec162)
+ * Config saved to config.json and https://gist.github.com/23d4836115a7f1cea15090f28a1ec162
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')

4f6f6f8e7059126cfa3e0afa09cfdcf2b10ced1f
beluoctavian <beluoctavian@gmail.com>
2016-08-12 11:57:08 +0300

    Legislations migration

---
+++ docroot/modules/iucn/elis_consumer/src/Plugin/migrate/source/ElisConsumerDefaultSource.php
@@ -0,0 +1,173 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\elis_consumer\Plugin\migrate\source\ElisConsumerDefaultSource.
+ */
+
+
+namespace Drupal\elis_consumer\Plugin\migrate\source;
+
+include_once __DIR__ . '/../../../../elis_consumer.xml.inc';
+
+use Drupal\elis_consumer\ElisXMLConsumer;
+use Drupal\migrate\MigrateSkipRowException;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+use Drupal\migrate\Row;
+
+abstract class ElisConsumerDefaultSource extends SourcePluginBase {
+
+  protected $files_destination;
+  protected $source = NULL;
+  protected $data = array();
+
+  /**
+   * Return the url to the elis web service endpoint.
+   * The url should contain two important strings:
+   *  - SPAGE_QUERY_VALUE
+   *  - SPAGE_FIRST_VALUE
+   * E.g. http://www.ecolex.org/elis_isis3w.php?database=cou&search_type=page_search&spage_query=SPAGE_QUERY_VALUE&spage_first=SPAGE_FIRST_VALUE
+   * @return string
+   */
+  abstract protected function getElisServicePath();
+
+  /**
+   * The directory where files should be stored.
+   * @return string
+   */
+  abstract protected function getFilesDestination();
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    $this->files_destination = $this->getFilesDestination();
+
+    if (isset($configuration['files_destination'])) {
+      $this->files_destination = $configuration['files_destination'];
+    }
+    $directory = "public://{$this->files_destination}";
+    file_prepare_directory($directory, FILE_CREATE_DIRECTORY);
+
+    if (!empty($configuration['path'])) {
+      $path = $configuration['path'];
+    }
+    else {
+      $path = $this->getElisServicePath();
+    }
+
+    $encoding = 'iso-8859-15';
+    if (!empty($configuration['encoding'])) {
+      $encoding = $configuration['encoding'];
+    }
+    $this->source = new ElisXMLConsumer($path, 'id', $encoding, '2014-01', '');
+  }
+
+  /**
+   * Retrieve the migration item.
+   * @param $data
+   * @return array
+   */
+  public function getItem($data) {
+    $ret = array();
+    $parties = array();
+    $abstract = '';
+    foreach ($data as $field_name => $value) {
+      $value = (string) $value;
+      if (mb_detect_encoding($value) != 'UTF-8') {
+        $value = utf8_encode($value);
+      }
+      if (strpos($value, 'www.') === 0) {
+        $value = 'http://' . $value;
+      }
+      if ($field_name == 'abstract') {
+        $abstract .= $value . PHP_EOL;
+        continue;
+      }
+      else if ($field_name == 'party') {
+        $parties[] = $value;
+        continue;
+      }
+      else if (!empty($ret[$field_name])) {
+        if (is_array($ret[$field_name])) {
+          $ret[$field_name][] = $value;
+        }
+        else {
+          $ret[$field_name] = array($ret[$field_name], $value);
+        }
+      }
+      else {
+        if ($field_name == 'titleOfText') {
+          $ret['titleOfText_original'] = $value;
+        }
+        $ret[$field_name] = $value;
+      }
+    }
+    $ret['abstract'] = $abstract;
+    $ret['parties'] = $parties;
+    $ret['obsolete'] = !empty($data->obsolete) && $data->obsolete == 'true';
+    return $ret;
+  }
+
+  protected function getData() {
+    if (empty($this->data)) {
+      $rows = $this->source->getData();
+      foreach($rows as $id => $row) {
+        if (!empty((string)$row->projectInformation) && strtoupper((string)$row->projectInformation) == 'WILD') {
+          $this->data[$id] = $row;
+        }
+      }
+    }
+    return $this->data;
+  }
+
+  public function count($refresh = FALSE) {
+    return 'n/a';
+  }
+
+  public function getIds() {
+    return array(
+      'id' => array('type' => 'string')
+    );
+  }
+
+  protected function initializeIterator() {
+    $data = $this->getData();
+    foreach($data as &$doc) {
+      $doc = $this->getItem($doc);
+    }
+    $iterator = new \ArrayIterator($data);
+    return $iterator;
+  }
+
+  public function getTitle(Row $row) {
+    if (empty($titleOfText = $row->getSourceProperty('titleOfText')) &&
+      empty($titleOfText = $row->getSourceProperty('titleOfTextSp')) &&
+      empty($titleOfText = $row->getSourceProperty('titleOfTextFr')) &&
+      empty($titleOfText = $row->getSourceProperty('titleOfTextOther'))) {
+      return NULL;
+    }
+    return $titleOfText;
+  }
+
+  protected function fixDateFields(Row &$row, array $sourceFields) {
+    foreach ($sourceFields as $field) {
+      $p = preg_match('/(\d\d\d\d)\-(\d\d)\-00/', $row->getSourceProperty($field), $matches);
+      if ($p) {
+        $month = $matches[2] != '00' ? $matches[2] : '01';
+        $row->setSourceProperty($field, "{$matches[1]}-{$month}-01");
+      }
+    }
+  }
+
+  public function prepareRow(Row $row) {
+    $titleOfText = $this->getTitle($row);
+    if (empty($titleOfText)) {
+      throw new MigrateSkipRowException("Record title cannot be NULL. (id:{$row->getSourceProperty('id')})");
+    }
+    $row->setSourceProperty('titleOfText', $titleOfText);
+    if (empty($row->getSourceProperty('titleOfTextShort'))) {
+      $row->setSourceProperty('titleOfTextShort', substr($titleOfText, 0, 255));
+    }
+  }
+
+}
---
+++ docroot/modules/iucn/elis_consumer/src/Plugin/migrate/source/ElisConsumerLegislationsSource.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\elis_consumer\Plugin\migrate\source\ElisConsumerLegislationsSource.
+ */
+
+
+namespace Drupal\elis_consumer\Plugin\migrate\source;
+
+include_once __DIR__ . '/../../../../elis_consumer.xml.inc';
+
+use Drupal\migrate\Row;
+
+
+/**
+ * Migrate legislations from ELIS database.
+ *
+ * @MigrateSource(
+ *   id = "elis_consumer_legislations"
+ * )
+ */
+class ElisConsumerLegislationsSource extends ElisConsumerDefaultSource {
+
+  protected function getElisServicePath() {
+    return 'http://www2.ecolex.org/elis_isis3w.php?database=legal&search_type=page_search&table=all&format_name=@xmlexp&lang=xmlf&page_header=@xmlh&spage_query=SPAGE_QUERY_VALUE&spage_first=SPAGE_FIRST_VALUE';
+  }
+
+  protected function getFilesDestination() {
+    return 'legislations';
+  }
+
+  public function __toString() {
+    return 'Migrate legislations from ELIS having `projectInformation` = `WILD`';
+  }
+
+  public function fields() {
+    return array(
+      'id' => 'Remote primary key',
+      'isisMfn' => 'Isis number',
+      'dateOfEntry' => 'Date of entry',
+      'dateOfModification' => 'Date of modification',
+      'dateOfText' => 'Date of text',
+      'titleOfText' => 'Title of text',
+      'titleOfTextShort' => 'Title of text short',
+      'titleOfText_original' => 'Title of text in english',
+      'titleOfText_languages' => 'Languages of titleOfText field',
+      'titleOfTextOther' => 'Title of text in another language',
+      'titleOfTextShortOther' => 'Short title of text in another language',
+      'country' => 'Country',
+      'subject' => 'Subject',
+      'languageOfDocument' => 'Language',
+      'linkToFullText' => 'Link to the full text',
+      'linkToFullText_languages' => 'Languages of linkToFullText field',
+      'linkToFullTextOther' => 'Link to full text in another language',
+      'typeOfText' => 'Type of text',
+      'region' => 'Geopolitical region',
+    );
+  }
+
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+    // Used str_replace('server2.php/', '', ...) because there is a bug in the urls from ELIS
+    $linkToFullText = str_replace('server2.php/', '', $row->getSourceProperty('linkToFullText'));
+    $row->setSourceProperty('linkToFullText', $linkToFullText);
+    $this->fixDateFields($row, [
+      'dateOfEntry',
+      'dateOfModification',
+      'dateOfText',
+    ]);
+    return TRUE;
+  }
+}

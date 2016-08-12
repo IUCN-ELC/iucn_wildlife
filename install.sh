088de224448cc29597efac82de50df605b988c45
beluoctavian <beluoctavian@gmail.com>
2016-08-12 11:57:38 +0300

    Do not enable simpletest module by default

--- install.sh
+++ install.sh
@@ -24,9 +24,6 @@ drush updatedb
 echo "Resetting admin password..."
 drush user-password iucn --password="password"
 
-echo "Enabling simpletest module..."
-drush en -y simpletest
-
 echo "Reindexing Solr ..."
 drush sapi-c
 drush sapi-i

77897e9c7def24913341ccbf2da5bf4d71732188
beluoctavian <beluoctavian@gmail.com>
2016-08-26 18:38:54 +0300

    Add config for cited treaties url

--- docroot/modules/iucn/iucn_search/src/Form/IucnSettingsForm.php
+++ docroot/modules/iucn/iucn_search/src/Form/IucnSettingsForm.php
@@ -41,15 +41,22 @@ class IucnSettingsForm extends ConfigFormBase {
     $form['reference_to_ecolex_pattern'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Reference to ECOLEX PATTERN'),
-      '#description' => $this->t('e.g. http://ecolex.eaudeweb.ro/details/court-decision/$RECID'),
+      '#description' => $this->t('e.g. http://ecolex.eaudeweb.ro/treaty/details/$RECID'),
       '#default_value' => $config->get('reference_to_ecolex_pattern'),
     );
 
     $form['reference_to_legislation_pattern'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Reference to legislation PATTERN'),
-      '#description' => $this->t('e.g. http://www.ecolex.org/ecolex/ledge/view/RecordDetails?id=$RECID&index=documents'),
+      '#description' => $this->t('e.g. http://ecolex.eaudeweb.ro/treaty/details/$RECID'),
       '#default_value' => $config->get('reference_to_legislation_pattern'),
+    );
+
+    $form['reference_to_treaty_pattern'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Reference to treaty PATTERN'),
+      '#description' => $this->t('e.g. http://ecolex.eaudeweb.ro/treaty/details/$RECID'),
+      '#default_value' => $config->get('reference_to_treaty_pattern'),
     );
 
     return parent::buildForm($form, $form_state);
@@ -62,6 +69,7 @@ class IucnSettingsForm extends ConfigFormBase {
     $this->config('iucn_search.settings')
       ->set('reference_to_ecolex_pattern', $form_state->getValue('reference_to_ecolex_pattern'))
       ->set('reference_to_legislation_pattern', $form_state->getValue('reference_to_legislation_pattern'))
+      ->set('reference_to_treaty_pattern', $form_state->getValue('reference_to_treaty_pattern'))
       ->save();
 
     parent::submitForm($form, $form_state);

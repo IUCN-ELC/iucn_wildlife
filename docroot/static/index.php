31d6a64728c517a667a09a294cc3c4d5eb3918e8
Octavian Belu <beluoctavian@gmail.com>
2016-11-22 15:25:39 +0200

    Delete static directory

--- docroot/static/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filename = 'elis_isis3w.php?' . $_SERVER['QUERY_STRING'];
-if (is_readable($filename)) {
-  print file_get_contents($filename);
-}
-else {
-  print "<result db=\"Faolex\"> <error> File not found $filename </error></result>";
-}

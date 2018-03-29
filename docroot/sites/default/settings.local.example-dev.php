53aec3a5b5a6edfec73958c37452368150b060c2
Cristian Romanescu <cristian@romanescu.ro>
2018-03-29 15:12:18 +0300

    Adding default configuration examples for production and development

---
+++ docroot/sites/default/settings.local.example-dev.php
@@ -0,0 +1,40 @@
+<?php
+
+/** Enable local development services. */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+/** Show all error messages */
+$config['system.logging']['error_level'] = 'all';
+
+/** Disable CSS/JS aggregation. */
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+/** Disable the render cache (this includes the page cache). */
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+
+/** Disable Dynamic Page Cache. */
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+/** Allow test modules and themes to be installed. */
+$settings['extension_discovery_scan_tests'] = TRUE;
+
+/** Enable access to rebuild.php.  */
+$settings['rebuild_access'] = TRUE;
+
+/** Views show SQL and performance on dev */
+$config['views.settings']['ui']['show']['sql_query']['enabled'] = TRUE;
+$config['views.settings']['ui']['show']['performance_statistics'] = TRUE;
+
+$databases['default']['default'] = [
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+$settings['trusted_host_patterns'] = ['wildlex.local'];
---
+++ docroot/sites/default/settings.local.example-prod.php
@@ -0,0 +1,35 @@
+<?php
+/** Configure database connection */
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+/** Which environment type are we running? */
+$settings['environment'] = 'live'; // 'live', 'test' or 'dev'
+
+/** Hash salt used to protect sensitive data */
+$settings['hash_salt'] = '@TODO - Add here a string of about 70 characters';
+
+/** Add here the known domains for this Drupal */
+$settings['trusted_host_patterns'] = ['(www\.)?wildlex.org'];
+
+/** Hide errors to users */
+$config['system.logging']['error_level'] = 'hide';
+
+/** Set temporary path */
+$config['system.file']['path']['temporary'] = '/tmp';
+
+/** Performance tuning */
+$config['system.performance']['cache']['page']['max_age'] = 1000;
+$config['system.performance']['css']['preprocess'] = TRUE;
+$config['system.performance']['js']['preprocess'] = TRUE;
+
+// Disabling stage file proxy if enabled on production
+$config['stage_file_proxy.settings']['origin'] = FALSE;

480e33d349451452a965ad18c47b9c5683315dcc
Octavian Belu <beluoctavian@gmail.com>
2018-03-29 12:28:11 +0300

    Update prod root path

--- drush/aliases/wildlex-aliases.drushrc.php
+++ drush/aliases/wildlex-aliases.drushrc.php
@@ -1,6 +1,6 @@
 <?php
 $aliases['prod'] = array(
-  'root' => '/var/www/html/wildlex/docroot',
+  'root' => '/var/local/wildlex/docroot',
   'uri' => 'http://www.wildlex.org',
   'remote-host' => 'www.wildlex.org',
   'remote-user' => '',

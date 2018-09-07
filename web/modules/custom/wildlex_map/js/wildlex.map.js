8fcdfce77e05b1759f3ad47101393baea50674b2
vadimski <vadeemk@gmail.com>
2018-09-07 16:59:47 +0300

    refs #5349 - style

--- web/modules/custom/wildlex_map/js/wildlex.map.js
+++ web/modules/custom/wildlex_map/js/wildlex.map.js
@@ -88,35 +88,5 @@ new Datamap({
 
 
 
-/*var map = new Datamap({
-  element: document.getElementById('wildlex_map_container'),
-  fills: {
-    HIGH: '#afafaf',
-    LOW: '#123456',
-    MEDIUM: 'blue',
-    UNKNOWN: 'rgb(0,0,0)',
-    defaultFill: 'green'
-  },
-  data: {
-    IRL: {
-      fillKey: 'LOW',
-      numberOfThings: 2002
-    },
-    USA: {
-      fillKey: 'MEDIUM',
-      numberOfThings: 10381
-    }
-  },
-  geographyConfig: {
-    popupTemplate: function(geo, data) {
-      return ['<div class="hoverinfo"><strong>',
-        'Number of things in ' + geo.properties.name,
-        ': ' + data.numberOfThings,
-        '</strong></div>'].join('');
-    }
-  }
-
-});*/
-
 
 

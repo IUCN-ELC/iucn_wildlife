b7a55951ccf636cfd107fae0b0b73a88f6e4c3d8
vadimski <vadeemk@gmail.com>
2019-01-21 17:47:45 +0200

    refs #5569 - Tree folders structure to migrated files, mirror from source server

--- web/modules/custom/elis_consumer/src/Plugin/migrate/process/UrlFile.php
+++ web/modules/custom/elis_consumer/src/Plugin/migrate/process/UrlFile.php
@@ -8,13 +8,10 @@
 namespace Drupal\elis_consumer\Plugin\migrate\process;
 
 use Drupal\field\Entity\FieldConfig;
-use Drupal\file\FileInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
-use Drupal\migrate\Annotation\MigrateProcessPlugin;
-
 
 /**
  * This plugin downloads remote file and saves it as Drupal file.
@@ -24,94 +21,127 @@ use Drupal\migrate\Annotation\MigrateProcessPlugin;
  * )
  */
 class UrlFile extends ProcessPluginBase {
-
-  /**
+    /**
    * {@inheritdoc}
    */
-  public function transform($url, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $url = trim($url);
-    if (empty($url)) {
-      return NULL;
-    }
-    $bundle = $row->getDestination()['type'];
-    $fi = FieldConfig::loadByName('node', $bundle, $destination_property);
-    $extensions = $fi->getSetting('file_extensions');
-    if (!empty($extensions)) {
-      $extensions = explode(' ', $extensions);
-    }
-    else {
-      $extensions = array();
-    }
-    // Not allowed
-    $pi = pathinfo($url, PATHINFO_EXTENSION);
-    if (!in_array($pi, $extensions)) {
-      $ext = implode(',', $extensions);
-      $migrate_executable->saveMessage("Invalid extension: `$pi` (allowed:$ext) for " . $url . "\n", MigrationInterface::MESSAGE_WARNING);
-      return NULL;
+    public function transform($url,
+        MigrateExecutableInterface $migrate_executable,
+        Row $row,
+        $destination_property
+    ) {
+        $url = trim($url);
+        if (empty($url)) {
+            return null;
+        }
+
+        $bundle = $row->getDestination()['type'];
+        $fi = FieldConfig::loadByName('node', $bundle, $destination_property);
+
+        // Get the allowed file extensions.
+        $ext = $fi->getSetting('file_extensions');
+        $extensions = $ext ? explode(' ', $ext) : [];
+
+        // Check if file extension is allowed.
+        $pi = pathinfo($url, PATHINFO_EXTENSION);
+        if (!in_array($pi, $extensions)) {
+            $ext = implode(',', $extensions);
+            $migrate_executable->saveMessage(
+                "Invalid extension: `$pi` (allowed:$ext) for " . $url . "\n",
+                MigrationInterface::MESSAGE_WARNING
+            );
+            return null;
+        }
+
+        $filename = basename($url);
+        $path = $fi->getSetting('file_directory') ?? '';
+
+        // Keep the original tree structure for all imported files.
+        $tree = explode('server2neu.php/', $url);
+        if (isset($tree[1])) {
+            $tree_path = '/' . str_replace($filename, '', $tree[1]) . '/';
+            $realpath = \Drupal::service('file_system')
+                ->realpath("public://$path") . $tree_path;
+            if (!file_exists($realpath)) {
+                mkdir($realpath, 0777, true);
+            }
+            $path .= $tree_path;
+        }
+
+        // Create the file.
+        if ($file = $this->createFile($url, $path, $filename, $migrate_executable)) {
+            return $file;
+        }
+
+        $migrate_executable->saveMessage("Could not append to $destination_property, file at $url", MigrationInterface::MESSAGE_WARNING);
+        return null;
     }
 
-    $filename = basename($url);
-    $path = $fi->getSetting('file_directory');
-    $path = !empty($path) ? $path . '/' : '';
-    $file = $this->createFile($url, $path, $filename, $migrate_executable);
-    if (!$file) {
-      $migrate_executable->saveMessage("Could not append to $destination_property, file at $url", MigrationInterface::MESSAGE_WARNING);
+    /**
+    * {@inheritdoc}
+    */
+    public function multiple()
+    {
+      return false;
     }
-    return $file;
-  }
 
 
-  /**
-   * {@inheritdoc}
-   */
-  public function multiple() {
-    return FALSE;
-  }
+    /**
+    * @param string $url
+    * @return integer
+    *  File ID (fid)
+    */
+    protected function createFile(
+        $url,
+        $destination_path,
+        $filename,
+        $migrate_executable
+    ) {
+        if ($data = $this->download($url)) {
+            // Ecolex specific - does not return 404, but 200 with 404 page.
+            if (preg_match('/Server Error/', $data)
+                || preg_match('/HTTP Status 404/', $data)
+            ) {
+                return null;
+            }
 
+            if (!file_prepare_directory(
+                $destination_path,
+                FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS
+            )) {
+                $migrate_executable->saveMessage("$destination_path is not writable", MigrationInterface::MESSAGE_WARNING);
+            }
 
-  /**
-   * @param string $url
-   * @return integer
-   *  File ID (fid)
-   */
-  protected function createFile($url, $destination_path, $filename, $migrate_executable) {
-    if ($data = $this->download($url)) {
-      // Ecolex specific - does not return 404, but 200 with 404 page :rofl:
-      if (preg_match('/Server Error/', $data) || preg_match('/HTTP Status 404/', $data)) {
-        return NULL;
-      }
-      if (!file_prepare_directory($destination_path, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS)) {
-        $migrate_executable->saveMessage("$destination_path is not writable", MigrationInterface::MESSAGE_WARNING);
-      }
-      $target = 'public://' . $destination_path . '/' . rawurldecode($filename);
-      /** @var FileInterface $file */
-      if ($file = file_save_data($data, $target, FILE_EXISTS_REPLACE)) {
-        return $file->id();
-      }
+            $target = 'public://' . $destination_path . '/' . rawurldecode($filename);
+            if ($file = file_save_data($data, $target, FILE_EXISTS_REPLACE)) {
+                return $file->id();
+            }
+        }
+        return null;
     }
-    return NULL;
-  }
 
-
-  protected function download($url, $headers = array()) {
-    if (empty($url)) {
-      return NULL;
+    /**
+     * {@inheritdoc}
+     */
+    protected function download($url, $headers = [])
+    {
+        if (empty($url)) {
+            return null;
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_NOBODY, 0);
+        $ret = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        if ($info['http_code'] != 200) {
+            $ret = null;
+        }
+        curl_close($ch);
+        return $ret;
     }
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-    curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_NOBODY, 0);
-    $ret = curl_exec($ch);
-    $info = curl_getinfo($ch);
-    if ($info['http_code'] != 200) {
-      $ret = NULL;
-    }
-    curl_close($ch);
-    return $ret;
-  }
 }

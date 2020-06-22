51f6aedbb5eea55ae8abb419f1efcc7d4d0ea2c0
leontin.caliman <leontin.caliman@eaudeweb.ro>
2020-06-22 10:13:44 +0300

    refs #8530 update core

--- web/sites/example.settings.local.php
+++ web/sites/example.settings.local.php
@@ -131,15 +131,15 @@ $settings['rebuild_access'] = TRUE;
 $settings['skip_permissions_hardening'] = TRUE;
 
 /**
- * Exclude modules from configuration synchronisation.
+ * Exclude modules from configuration synchronization.
  *
  * On config export sync, no config or dependent config of any excluded module
  * is exported. On config import sync, any config of any installed excluded
  * module is ignored. In the exported configuration, it will be as if the
  * excluded module had never been installed. When syncing configuration, if an
  * excluded module is already installed, it will not be uninstalled by the
- * configuration synchronisation, and dependent configuration will remain
- * intact. This affects only configuration synchronisation; single import and
+ * configuration synchronization, and dependent configuration will remain
+ * intact. This affects only configuration synchronization; single import and
  * export of configuration are not affected.
  *
  * Drupal does not validate or sanity check the list of excluded modules. For
